1e90f55210982e8e70e15b27805ee109a497a7a9
Lukas Fellechner <lukas.fellechner@gmx.net>
2018-04-15 22:08:02 +0200

    Use ffmpeg to decode ass

--- FFmpegInterop/Source/SubtitleProviderSsaAss.h
+++ FFmpegInterop/Source/SubtitleProviderSsaAss.h
@@ -33,26 +33,36 @@ namespace FFmpegInterop
 
 		virtual IMediaCue^ CreateCue(AVPacket* packet, TimeSpan* position, TimeSpan *duration) override
 		{
-			auto str = utf8_to_wstring(std::string((char*)packet->data, packet->size));
-
-			// TODO we could parse style definitions from extradata and forward bold and italic
-
-			// strip effects from string
-			while (true)
+			AVSubtitle subtitle;
+			int gotSubtitle = 0;
+			auto result = avcodec_decode_subtitle2(m_pAvCodecCtx, &subtitle, &gotSubtitle, packet);
+			if (result > 0 && gotSubtitle && subtitle.num_rects > 0)
 			{
-				auto nextEffect = str.find('{');
-				if (nextEffect >= 0)
+				auto str = utf8_to_wstring(std::string(subtitle.rects[0]->ass));
+
+				// TODO we could parse style definitions from extradata and forward bold and italic
+
+				// strip effects from string
+				while (true)
 				{
-					auto endEffect = str.find('}', nextEffect);
-					if (endEffect > nextEffect)
+					auto nextEffect = str.find('{');
+					if (nextEffect >= 0)
 					{
-						if (endEffect < str.length() - 1)
+						auto endEffect = str.find('}', nextEffect);
+						if (endEffect > nextEffect)
 						{
-							str = str.substr(0, nextEffect).append(str.substr(endEffect + 1));
+							if (endEffect < str.length() - 1)
+							{
+								str = str.substr(0, nextEffect).append(str.substr(endEffect + 1));
+							}
+							else
+							{
+								str = str.substr(0, nextEffect);
+							}
 						}
 						else
 						{
-							str = str.substr(0, nextEffect);
+							break;
 						}
 					}
 					else
@@ -60,28 +70,24 @@ namespace FFmpegInterop
 						break;
 					}
 				}
-				else
+
+				// TODO we need to parse header to find out how many commas to skip. Acutal text could contain commas as well!
+				auto lastComma = str.find_last_of(',');
+				if (lastComma > 0 && lastComma < str.length() - 1)
 				{
-					break;
+					str = str.substr(lastComma + 1);
+					auto timedText = convertFromString(str);
+
+					TimedTextCue^ cue = ref new TimedTextCue();
+					cue->CueRegion = m_config->SubtitleRegion;
+					cue->CueStyle = m_config->SubtitleStyle;
+
+					TimedTextLine^ textLine = ref new TimedTextLine();
+					textLine->Text = timedText;
+					cue->Lines->Append(textLine);
+
+					return cue;
 				}
-			}
-
-			// now text should be last element of comma separated list
-			auto lastComma = str.find_last_of(',');
-			if (lastComma > 0 && lastComma < str.length() - 1)
-			{
-				str = str.substr(lastComma + 1);
-				auto timedText = convertFromString(str);
-
-				TimedTextCue^ cue = ref new TimedTextCue();
-				cue->CueRegion = m_config->SubtitleRegion;
-				cue->CueStyle = m_config->SubtitleStyle;
-
-				TimedTextLine^ textLine = ref new TimedTextLine();
-				textLine->Text = timedText;
-				cue->Lines->Append(textLine);
-
-				return cue;
 			}
 
 			return nullptr;
