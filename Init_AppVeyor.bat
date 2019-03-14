3e7451d95f76fbe6e1a542be633c42ddb13851bf
Lukas Fellechner <lukas.fellechner@gmx.net>
2019-03-14 20:46:27 +0100

    Add build init file for AppVeyor CI

---
+++ Init_AppVeyor.bat
@@ -0,0 +1,7 @@
+if exist ffmpeg\Build\Windows10\x86\nul goto end
+
+del temp.zip
+curl -L -o temp.zip "https://onedrive.live.com/download?cid=8DFA3705CA6CB7CC&resid=8DFA3705CA6CB7CC%%21128910&authkey=AI1twiJV1hLTTiw" 
+"C:\Program Files\7-zip\7z" x temp.zip
+
+:end
