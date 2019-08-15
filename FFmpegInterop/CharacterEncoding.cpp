075c56e8d3c15dd9ca631d8e8b1a95d624be2d45
Lukas Fellechner <lukas.fellechner@gmx.net>
2019-08-15 13:13:28 +0200

    Fix race condition in character encoding initialization

--- FFmpegInterop/CharacterEncoding.cpp
+++ FFmpegInterop/CharacterEncoding.cpp
@@ -4,4 +4,7 @@ namespace FFmpegInterop
 {
 
 	Vector<CharacterEncoding^>^ FFmpegInterop::CharacterEncoding::internalMap;
+	Windows::Foundation::Collections::IVectorView<CharacterEncoding^>^ FFmpegInterop::CharacterEncoding::internalView;
+	std::mutex FFmpegInterop::CharacterEncoding::mutex;
+
 }
