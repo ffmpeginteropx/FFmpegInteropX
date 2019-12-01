93d389e2f3cbb70bad9d585149df7d3ce9cd0e51
Julio C. Rocha <julio@rochsquadron.net>
2019-12-01 02:16:21 -0800

    Update arguments.

--- Build-FFmpeg.ps1
+++ Build-FFmpeg.ps1
@@ -10,6 +10,8 @@ param(
         14.16
         14.16.27023
         14.23.27820
+
+        Note. The PlatformToolset will be inferred from this value ('v141', 'v142'...)
     #>
     [version] $VcVersion = '14.1',
 
@@ -20,126 +22,137 @@ param(
         10.0.17763.0
         10.0.18362.0
     #>
-    [version] $WindowsTargetPlatformVersion = '10.0.15063.0',
+    [version] $WindowsTargetPlatformVersion = '10.0.18362.0',
 
     [ValidateSet('Debug', 'Release')]
     [string] $Configuration = 'Release',
 
     [System.IO.DirectoryInfo] $VSInstallerFolder = "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer",
 
+    # Set the search criteria for VSWHERE.EXE.
+    [string[]] $VsWhereCriteria = '-latest',
+
     [System.IO.FileInfo] $Msys2Bin = 'C:\msys64\usr\bin\bash.exe'
 )
 
-$buildFunctionScript = {
+function Build-Platform {
+    param (
+        [System.IO.DirectoryInfo] $SolutionDir,
+        [string] $Platform,
+        [string] $Configuration,
+        [version] $WindowsTargetPlatformVersion,
+        [version] $VcVersion,
+        [string] $VsLatestPath,
+        [string] $Msys2Bin = 'C:\msys64\usr\bin\bash.exe'
+    )
 
-    <#
-        Meant to run in its own process (job).
-    #>
-    function Build-Platform {
-        param (
-            [System.IO.DirectoryInfo] $SolutionDir,
-            [string] $Platform,
-            [string] $Configuration,
-            [version] $WindowsTargetPlatformVersion,
-            [version] $VcVersion,
-            [string] $VsLatestPath,
-            [string] $Msys2Bin = 'C:\msys64\usr\bin\bash.exe'
-        )
+    $PSBoundParameters | Out-String
 
-        $PSBoundParameters | Out-String
-
-        $vcvarsArchs = @{
-            'x86' = @{
-                'x86'   = 'x86'
-                'x64'   = 'x86_amd64'
-                'ARM'   = 'x86_arm'
-                'ARM64' = 'x86_arm64'
-            }
-        
-            'AMD64' = @{
-                'x86'   = 'amd64_x86'
-                'x64'   = 'amd64'
-                'ARM'   = 'amd64_arm'
-                'ARM64' = 'amd64_arm64'
-            }
+    $vcvarsArchs = @{
+        'x86' = @{
+            'x86'   = 'x86'
+            'x64'   = 'x86_amd64'
+            'ARM'   = 'x86_arm'
+            'ARM64' = 'x86_arm64'
         }
+    
+        'AMD64' = @{
+            'x86'   = 'amd64_x86'
+            'x64'   = 'amd64'
+            'ARM'   = 'amd64_arm'
+            'ARM64' = 'amd64_arm64'
+        }
+    }
 
-        Write-Host "Building FFmpeg for Windows 10 apps ${Platform}..."
+    Write-Host "Building FFmpeg for Windows 10 apps ${Platform}..."
 
-        # Load environment from VCVARS.
-        $vcvarsArch = $vcvarsArchs[$env:PROCESSOR_ARCHITECTURE][$Platform]
+    # Load environment from VCVARS.
+    $vcvarsArch = $vcvarsArchs[$env:PROCESSOR_ARCHITECTURE][$Platform]
 
-        CMD /c "`"$VsLatestPath\VC\Auxiliary\Build\vcvarsall.bat`" $vcvarsArch uwp $WindowsTargetPlatformVersion -vcvars_ver=$VcVersion && SET" | . {
-            PROCESS {
-                if ($_ -match '^([^=]+)=(.*)') {
-                    if ($Matches[1] -notin 'HOME') {
-                        Set-Item -Path "Env:\$($Matches[1])" -Value $Matches[2]
-                    }
+    CMD /c "`"$VsLatestPath\VC\Auxiliary\Build\vcvarsall.bat`" $vcvarsArch uwp $WindowsTargetPlatformVersion -vcvars_ver=$VcVersion && SET" | . {
+        PROCESS {
+            if ($_ -match '^([^=]+)=(.*)') {
+                if ($Matches[1] -notin 'HOME') {
+                    Set-Item -Path "Env:\$($Matches[1])" -Value $Matches[2]
                 }
             }
         }
+    }
 
-        # 14.16.27023 => v141
-        $platformToolSet = "v$($VcVersion.Major)$("$VcVersion.Minor"[0])"
+    # 14.16.27023 => v141
+    $platformToolSet = "v$($VcVersion.Major)$("$VcVersion.Minor"[0])"
 
-        New-Item -ItemType Directory -Force $SolutionDir\Libs\Build\$Platform -OutVariable libs
+    New-Item -ItemType Directory -Force $SolutionDir\Libs\Build\$Platform -OutVariable libs
 
-        ('lib', 'licenses', 'include', 'build') | ForEach-Object {
-            New-Item -ItemType Directory -Force $libs\$_
-        }
-        # Clean platform-specific build dir.
-        Remove-Item -Force -Recurse $libs\build\*
+    ('lib', 'licenses', 'include', 'build') | ForEach-Object {
+        New-Item -ItemType Directory -Force $libs\$_
+    }
+    # Clean platform-specific build dir.
+    Remove-Item -Force -Recurse $libs\build\*
 
+    try {
         MSBuild.exe $SolutionDir\Libs\zlib\SMP\libzlib.vcxproj `
             /p:OutDir="$libs\build\" `
             /p:Configuration="${Configuration}WinRT" `
             /p:Platform=$Platform `
             /p:WindowsTargetPlatformVersion=$WindowsTargetPlatformVersion `
             /p:PlatformToolset=$platformToolSet
+    }
+    catch {
+        Exit
+    }
 
-        Get-ChildItem -Recurse -Include '*.h' $libs\build\libzlib\include | Copy-Item -Destination $libs\include\
-        Copy-Item -Recurse $libs\build\libzlib\licenses\* -Destination $libs\licenses\
-        Copy-Item $libs\build\libzlib\lib\$Platform\libzlib_winrt.lib $libs\lib\zlib.lib
-        Copy-Item $libs\build\libzlib\lib\$Platform\libzlib_winrt.pdb $libs\lib\zlib.pdb
+    Get-ChildItem -Recurse -Include '*.h' $libs\build\libzlib\include | Copy-Item -Destination $libs\include\
+    Copy-Item -Recurse $libs\build\libzlib\licenses\* -Destination $libs\licenses\
+    Copy-Item $libs\build\libzlib\lib\$Platform\libzlib_winrt.lib $libs\lib\zlib.lib
+    Copy-Item $libs\build\libzlib\lib\$Platform\libzlib_winrt.pdb $libs\lib\zlib.pdb
 
+    try {
         MSBuild.exe $SolutionDir\Libs\bzip2\SMP\libbz2.vcxproj `
             /p:OutDir="$libs\build\" `
             /p:Configuration="${Configuration}WinRT" `
             /p:Platform=$Platform `
             /p:WindowsTargetPlatformVersion=$WindowsTargetPlatformVersion `
             /p:PlatformToolset=$platformToolSet
+    }
+    catch {
+        Exit
+    }
 
-        Get-ChildItem -Recurse -Include '*.h' $libs\build\libbz2\include | Copy-Item -Destination $libs\include\
-        Copy-Item -Recurse $libs\build\libbz2\licenses\* -Destination $libs\licenses\
-        Copy-Item $libs\build\libbz2\lib\$Platform\libbz2_winrt.lib $libs\lib\bz2.lib
-        Copy-Item $libs\build\libbz2\lib\$Platform\libbz2_winrt.pdb $libs\lib\bz2.pdb
+    Get-ChildItem -Recurse -Include '*.h' $libs\build\libbz2\include | Copy-Item -Destination $libs\include\
+    Copy-Item -Recurse $libs\build\libbz2\licenses\* -Destination $libs\licenses\
+    Copy-Item $libs\build\libbz2\lib\$Platform\libbz2_winrt.lib $libs\lib\bz2.lib
+    Copy-Item $libs\build\libbz2\lib\$Platform\libbz2_winrt.pdb $libs\lib\bz2.pdb
 
+    try {
         MSBuild.exe $SolutionDir\Libs\libiconv\SMP\libiconv.vcxproj `
             /p:OutDir="$libs\build\" `
             /p:Configuration="${Configuration}WinRT" `
             /p:Platform=$Platform `
             /p:WindowsTargetPlatformVersion=$WindowsTargetPlatformVersion `
             /p:PlatformToolset=$platformToolSet
-
-        Get-ChildItem -Recurse -Include '*.h' $libs\build\libiconv\include | Copy-Item -Destination $libs\include\
-        Copy-Item -Recurse $libs\build\libiconv\licenses\* -Destination $libs\licenses\
-        Copy-Item $libs\build\libiconv\lib\$Platform\libiconv_winrt.lib $libs\lib\iconv.lib
-        Copy-Item $libs\build\libiconv\lib\$Platform\libiconv_winrt.pdb $libs\lib\iconv.pdb
-
-        $env:LIB += ";${libs}\lib"
-        $env:INCLUDE += ";${libs}\include"
-
-        # Export full current PATH from environment into MSYS2
-        $env:MSYS2_PATH_TYPE = 'inherit'
-
-        # Build ffmpeg
-        & $Msys2Bin --login -x $SolutionDir\FFmpegConfig.sh Win10 $Platform
-
-        Start-Job -ScriptBlock { & $Msys2Bin --login -x $SolutionDir\FFmpegConfig.sh Win10 $Platform } | Receive-Job -Wait
-
-        # Copy PDBs to built binaries dir
-        Get-ChildItem -Recurse -Include '*.pdb' $SolutionDir\ffmpeg\Output\Windows10\$Platform | Copy-Item -Destination $SolutionDir\ffmpeg\Build\Windows10\$Platform\bin\
     }
+    catch {
+        Exit
+    }
+
+    Get-ChildItem -Recurse -Include '*.h' $libs\build\libiconv\include | Copy-Item -Destination $libs\include\
+    Copy-Item -Recurse $libs\build\libiconv\licenses\* -Destination $libs\licenses\
+    Copy-Item $libs\build\libiconv\lib\$Platform\libiconv_winrt.lib $libs\lib\iconv.lib
+    Copy-Item $libs\build\libiconv\lib\$Platform\libiconv_winrt.pdb $libs\lib\iconv.pdb
+
+    $env:LIB += ";${libs}\lib"
+    $env:INCLUDE += ";${libs}\include"
+
+    # Export full current PATH from environment into MSYS2
+    $env:MSYS2_PATH_TYPE = 'inherit'
+
+    # Build ffmpeg
+    & $Msys2Bin --login -x $SolutionDir\FFmpegConfig.sh Win10 $Platform
+
+    # Copy PDBs to built binaries dir
+    Get-ChildItem -Recurse -Include '*.pdb' $SolutionDir\ffmpeg\Output\Windows10\$Platform | `
+        Copy-Item -Destination $SolutionDir\ffmpeg\Build\Windows10\$Platform\bin\
 }
 
 if (! (Test-Path $PSScriptRoot\ffmpeg\configure)) {
@@ -147,29 +160,45 @@ if (! (Test-Path $PSScriptRoot\ffmpeg\configure)) {
     Exit
 }
 
-[System.IO.DirectoryInfo] $vsLatestPath = & "$VSInstallerFolder\vswhere.exe" -latest -property installationPath -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64
+if (!(Test-Path $Msys2Bin)) {
+
+    $msysFound = $false
+    @( 'C:\msys64', 'C:\msys' ) | ForEach-Object {
+        if (Test-Path $_) {
+            $Msys2Bin = "${_}\usr\bin\bash.exe"
+            $msysFound = $true
+
+            break
+        }
+    }
+
+    # Search for MSYS locations
+    if (! $msysFound) {
+        Write-Error "MSYS2 not found."
+        Exit;
+    }
+}
+
+[System.IO.DirectoryInfo] $vsLatestPath = `
+    & "$VSInstallerFolder\vswhere.exe" `
+    $VsWhereCriteria `
+    -property installationPath `
+    -products * `
+    -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64
 
 Write-Host "Visual Studio Installation folder: [$vsLatestPath]"
 
 $start = Get-Date
 
 foreach ($platform in $Platforms) {
-
-    #TODO: Pass named parameters and forget about args indexes.
-    Start-Job `
-        -InitializationScript $buildFunctionScript `
-        -ArgumentList $PSScriptRoot, $platform, $Configuration, $WindowsTargetPlatformVersion, $VcVersion, $vsLatestPath, $Msys2Bin `
-        -ScriptBlock {
-            Build-Platform `
-                -SolutionDir $args[0] `
-                -Platform $args[1] `
-                -Configuration $args[2] `
-                -WindowsTargetPlatformVersion $args[3] `
-                -VcVersion $args[4] `
-                -VsLatestPath $args[5] `
-                -Msys2Bin $args[6]
-        } `
-    | Receive-Job -Wait
+    Build-Platform `
+        -SolutionDir "${PSScriptRoot}\" `
+        -Platform $platform `
+        -Configuration 'Release' `
+        -WindowsTargetPlatformVersion $WindowsTargetPlatformVersion `
+        -VcVersion $VcVersion `
+        -VsLatestPath $vsLatestPath `
+        -Msys2Bin $Msys2Bin
 }
 
 Write-Host 'Time elapsed'
