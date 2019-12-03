24b871dc4300bfdc057e94d06e15557ff7727f5b
Julio C. Rocha <julio@rochsquadron.net>
2019-12-03 04:09:48 -0800

    Make ffmpeg output to Build/ and Target/

--- FFmpegConfig.sh
+++ FFmpegConfig.sh
@@ -1,26 +1,32 @@
 #!/bin/bash
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
+declare -A arch
+arch['x86']='x86'
+arch['x64']='x86_64'
+arch['ARM']='arm'
+arch['ARM64']='arm64'
+
 if [ "$1" == "Win10" ]; then
-    echo "Make Win10"
+    echo "Make $1"
 
     if [ "$2" == "x86" ]; then
-        echo "Make Win10 x86"
+        echo "Make $1 $2"
         pushd $DIR/ffmpeg
-		rm -rf Build/Windows10/x86
-        rm -rf Output/Windows10/x86
-        mkdir -p Output/Windows10/x86
-        cd Output/Windows10/x86
-        ../../../configure \
+        rm -rf $DIR/Target/$2/Release/ffmpeg-$1
+        rm -rf $DIR/Build/$2/Release/ffmpeg-$1
+        mkdir -p $DIR/Build/$2/Release/ffmpeg-$1
+        cd $DIR/Build/$2/Release/ffmpeg-$1
+        $DIR/ffmpeg/configure \
         --toolchain=msvc \
         --disable-programs \
         --disable-d3d11va \
         --disable-dxva2 \
-		--disable-encoders \
-		--disable-devices \
-		--disable-hwaccels \
+        --disable-encoders \
+        --disable-devices \
+        --disable-hwaccels \
         --disable-doc \
-        --arch=x86 \
+        --arch="${arch[$2]}" \
         --enable-shared \
         --enable-cross-compile \
         --enable-debug \
@@ -30,28 +36,28 @@ if [ "$1" == "Win10" ]; then
         --target-os=win32 \
         --extra-cflags="-MD -DWINAPI_FAMILY=WINAPI_FAMILY_APP -D_WIN32_WINNT=0x0A00" \
         --extra-ldflags="-APPCONTAINER WindowsApp.lib" \
-        --prefix=../../../Build/Windows10/x86
+        --prefix=$DIR/Target/$2/Release/ffmpeg-$1
         make -j8
         make install
         popd
 
     elif [ "$2" == "x64" ]; then
-        echo "Make Win10 x64"
+        echo "Make $1 $2"
         pushd $DIR/ffmpeg
-        rm -rf Build/Windows10/x64
-        rm -rf Output/Windows10/x64
-        mkdir -p Output/Windows10/x64
-        cd Output/Windows10/x64
-        ../../../configure \
+        rm -rf $DIR/Target/$2/Release/ffmpeg-$1
+        rm -rf $DIR/Build/$2/Release/ffmpeg-$1
+        mkdir -p $DIR/Build/$2/Release/ffmpeg-$1
+        cd $DIR/Build/$2/Release/ffmpeg-$1
+        $DIR/ffmpeg/configure \
         --toolchain=msvc \
         --disable-programs \
         --disable-d3d11va \
         --disable-dxva2 \
-		--disable-encoders \
-		--disable-devices \
-		--disable-hwaccels \
+        --disable-encoders \
+        --disable-devices \
+        --disable-hwaccels \
         --disable-doc \
-        --arch=x86_64 \
+        --arch="${arch[$2]}" \
         --enable-shared \
         --enable-cross-compile \
         --enable-debug \
@@ -61,28 +67,28 @@ if [ "$1" == "Win10" ]; then
         --target-os=win32 \
         --extra-cflags="-MD -DWINAPI_FAMILY=WINAPI_FAMILY_APP -D_WIN32_WINNT=0x0A00" \
         --extra-ldflags="-APPCONTAINER WindowsApp.lib" \
-        --prefix=../../../Build/Windows10/x64
+        --prefix=$DIR/Target/$2/Release/ffmpeg-$1
         make -j8
         make install
         popd
 
     elif [ "$2" == "ARM" ]; then
-        echo "Make Win10 ARM"
+        echo "Make $1 $2"
         pushd $DIR/ffmpeg
-        rm -rf Build/Windows10/ARM
-        rm -rf Output/Windows10/ARM
-        mkdir -p Output/Windows10/ARM
-        cd Output/Windows10/ARM
-        ../../../configure \
+        rm -rf $DIR/Target/$2/Release/ffmpeg-$1
+        rm -rf $DIR/Build/$2/Release/ffmpeg-$1
+        mkdir -p $DIR/Build/$2/Release/ffmpeg-$1
+        cd $DIR/Build/$2/Release/ffmpeg-$1
+        $DIR/ffmpeg/configure \
         --toolchain=msvc \
         --disable-programs \
         --disable-d3d11va \
         --disable-dxva2 \
-		--disable-encoders \
-		--disable-devices \
-		--disable-hwaccels \
+        --disable-encoders \
+        --disable-devices \
+        --disable-hwaccels \
         --disable-doc \
-        --arch=arm \
+        --arch="${arch[$2]}" \
         --as=armasm \
         --cpu=armv7 \
         --enable-thumb \
@@ -95,28 +101,28 @@ if [ "$1" == "Win10" ]; then
         --target-os=win32 \
         --extra-cflags="-MD -DWINAPI_FAMILY=WINAPI_FAMILY_APP -D_WIN32_WINNT=0x0A00 -D__ARM_PCS_VFP" \
         --extra-ldflags="-APPCONTAINER WindowsApp.lib" \
-        --prefix=../../../Build/Windows10/ARM
+        --prefix=$DIR/Target/$2/Release/ffmpeg-$1
         make -j8
         make install
         popd
 
     elif [ "$2" == "ARM64" ]; then
-        echo "Make Win10 ARM64"
+        echo "Make $1 $2"
         pushd $DIR/ffmpeg
-        rm -rf Build/Windows10/ARM64
-        rm -rf Output/Windows10/ARM64
-        mkdir -p Output/Windows10/ARM64
-        cd Output/Windows10/ARM64
-        ../../../configure \
+        rm -rf $DIR/Target/$2/Release/ffmpeg-$1
+        rm -rf $DIR/Build/$2/Release/ffmpeg-$1
+        mkdir -p $DIR/Build/$2/Release/ffmpeg-$1
+        cd $DIR/Build/$2/Release/ffmpeg-$1
+        $DIR/ffmpeg/configure \
         --toolchain=msvc \
         --disable-programs \
         --disable-d3d11va \
         --disable-dxva2 \
-		--disable-encoders \
-		--disable-devices \
-		--disable-hwaccels \
+        --disable-encoders \
+        --disable-devices \
+        --disable-hwaccels \
         --disable-doc \
-        --arch=arm64 \
+        --arch="${arch[$2]}" \
         --cpu=armv7 \
         --enable-thumb \
         --enable-shared \
@@ -128,7 +134,7 @@ if [ "$1" == "Win10" ]; then
         --target-os=win32 \
         --extra-cflags="-MD -DWINAPI_FAMILY=WINAPI_FAMILY_APP -D_WIN32_WINNT=0x0A00 -D__ARM_PCS_VFP" \
         --extra-ldflags="-APPCONTAINER WindowsApp.lib" \
-        --prefix=../../../Build/Windows10/ARM64
+        --prefix=$DIR/Target/$2/Release/ffmpeg-$1
         make -j8
         make install
         popd
@@ -136,67 +142,67 @@ if [ "$1" == "Win10" ]; then
     fi
 
 elif [ "$1" == "Win8.1" ]; then
-    echo "Make Win8.1"
+    echo "Make $1"
 
     if [ "$2" == "x86" ]; then
-        echo "Make Win8.1 x86"
+        echo "Make $1 $2"
         pushd $DIR/ffmpeg
-        rm -rf Output/Windows8.1/x86
-        mkdir -p Output/Windows8.1/x86
-        cd Output/Windows8.1/x86
-        ../../../configure \
+        rm -rf $DIR/Build/$2/Release/ffmpeg-$1
+        mkdir -p $DIR/Build/$2/Release/ffmpeg-$1
+        cd $DIR/Build/$2/Release/ffmpeg-$1
+        $DIR/ffmpeg/configure \
         --toolchain=msvc \
         --disable-programs \
         --disable-d3d11va \
         --disable-dxva2 \
         --disable-doc \
-        --arch=x86 \
+        --arch="${arch[$2]}" \
         --enable-shared \
         --enable-cross-compile \
         --enable-debug \
         --target-os=win32 \
         --extra-cflags="-MD -DWINAPI_FAMILY=WINAPI_FAMILY_PC_APP -D_WIN32_WINNT=0x0603" \
         --extra-ldflags="-APPCONTAINER" \
-        --prefix=../../../Build/Windows8.1/x86
+        --prefix=$DIR/Target/$2/Release/ffmpeg-$1
         make install
         popd
 
     elif [ "$2" == "x64" ]; then
-        echo "Make Win8.1 x64"
+        echo "Make $1 $2"
         pushd $DIR/ffmpeg
-        rm -rf Output/Windows8.1/x64
-        mkdir -p Output/Windows8.1/x64
-        cd Output/Windows8.1/x64
-        ../../../configure \
+        rm -rf $DIR/Build/$2/Release/ffmpeg-$1
+        mkdir -p $DIR/Build/$2/Release/ffmpeg-$1
+        cd $DIR/Build/$2/Release/ffmpeg-$1
+        $DIR/ffmpeg/configure \
         --toolchain=msvc \
         --disable-programs \
         --disable-d3d11va \
         --disable-dxva2 \
         --disable-doc \
-        --arch=x86_64 \
+        --arch="${arch[$2]}" \
         --enable-shared \
         --enable-cross-compile \
         --enable-debug \
         --target-os=win32 \
         --extra-cflags="-MD -DWINAPI_FAMILY=WINAPI_FAMILY_PC_APP -D_WIN32_WINNT=0x0603" \
         --extra-ldflags="-APPCONTAINER" \
-        --prefix=../../../Build/Windows8.1/x64
+        --prefix=$DIR/Target/$2/Release/ffmpeg-$1
         make install
         popd
 
     elif [ "$2" == "ARM" ]; then
-        echo "Make Win8.1 ARM"
+        echo "Make $1 $2"
         pushd $DIR/ffmpeg
-        rm -rf Output/Windows8.1/ARM
-        mkdir -p Output/Windows8.1/ARM
-        cd Output/Windows8.1/ARM
-        ../../../configure \
+        rm -rf $DIR/Build/$2/Release/ffmpeg-$1
+        mkdir -p $DIR/Build/$2/Release/ffmpeg-$1
+        cd $DIR/Build/$2/Release/ffmpeg-$1
+        $DIR/ffmpeg/configure \
         --toolchain=msvc \
         --disable-programs \
         --disable-d3d11va \
         --disable-dxva2 \
         --disable-doc \
-        --arch=arm \
+        --arch="${arch[$2]}" \
         --as=armasm \
         --cpu=armv7 \
         --enable-thumb \
@@ -205,29 +211,29 @@ elif [ "$1" == "Win8.1" ]; then
         --enable-debug \
         --target-os=win32 \
         --extra-cflags="-MD -DWINAPI_FAMILY=WINAPI_FAMILY_PC_APP -D_WIN32_WINNT=0x0603 -D__ARM_PCS_VFP" \
-        --extra-ldflags="-APPCONTAINER -MACHINE:ARM" \
-        --prefix=../../../Build/Windows8.1/ARM
+        --extra-ldflags="-APPCONTAINER -MACHINE:$2" \
+        --prefix=$DIR/Target/$2/Release/ffmpeg-$1
         make install
         popd
 
     fi
 
 elif [ "$1" == "Phone8.1" ]; then
-    echo "Make Phone8.1"
+    echo "Make $1"
 
     if [ "$2" == "ARM" ]; then
-        echo "Make Phone8.1 ARM"
+        echo "Make $1 $2"
         pushd $DIR/ffmpeg
-        rm -rf Output/WindowsPhone8.1/ARM
-        mkdir -p Output/WindowsPhone8.1/ARM
-        cd Output/WindowsPhone8.1/ARM
-        ../../../configure \
+        rm -rf $DIR/Build/$2/Release/ffmpeg-$1
+        mkdir -p $DIR/Build/$2/Release/ffmpeg-$1
+        cd $DIR/Build/$2/Release/ffmpeg-$1
+        $DIR/ffmpeg/configure \
         --toolchain=msvc \
         --disable-programs \
         --disable-d3d11va \
         --disable-dxva2 \
         --disable-doc \
-        --arch=arm \
+        --arch="${arch[$2]}" \
         --as=armasm \
         --cpu=armv7 \
         --enable-thumb \
@@ -236,82 +242,82 @@ elif [ "$1" == "Phone8.1" ]; then
         --enable-debug \
         --target-os=win32 \
         --extra-cflags="-MD -DWINAPI_FAMILY=WINAPI_FAMILY_PHONE_APP -D_WIN32_WINNT=0x0603 -D__ARM_PCS_VFP" \
-        --extra-ldflags="-APPCONTAINER -MACHINE:ARM -subsystem:console -opt:ref WindowsPhoneCore.lib RuntimeObject.lib PhoneAppModelHost.lib -NODEFAULTLIB:kernel32.lib -NODEFAULTLIB:ole32.lib" \
-        --prefix=../../../Build/WindowsPhone8.1/ARM
+        --extra-ldflags="-APPCONTAINER -MACHINE:$2 -subsystem:console -opt:ref WindowsPhoneCore.lib RuntimeObject.lib PhoneAppModelHost.lib -NODEFAULTLIB:kernel32.lib -NODEFAULTLIB:ole32.lib" \
+        --prefix=$DIR/Target/$2/Release/ffmpeg-$1
         make install
         popd
 
     elif [ "$2" == "x86" ]; then
-        echo "Make Phone8.1 x86"
+        echo "Make $1 $2"
         pushd $DIR/ffmpeg
-        rm -rf Output/WindowsPhone8.1/x86
-        mkdir -p Output/WindowsPhone8.1/x86
-        cd Output/WindowsPhone8.1/x86
-        ../../../configure \
+        rm -rf $DIR/Build/$2/Release/ffmpeg-$1
+        mkdir -p $DIR/Build/$2/Release/ffmpeg-$1
+        cd $DIR/Build/$2/Release/ffmpeg-$1
+        $DIR/ffmpeg/configure \
         --toolchain=msvc \
         --disable-programs \
         --disable-d3d11va \
         --disable-dxva2 \
         --disable-doc \
-        --arch=x86 \
+        --arch="${arch[$2]}" \
         --enable-shared \
         --enable-cross-compile \
         --target-os=win32 \
         --enable-debug \
         --extra-cflags="-MD -DWINAPI_FAMILY=WINAPI_FAMILY_PHONE_APP -D_WIN32_WINNT=0x0603" \
         --extra-ldflags="-APPCONTAINER -subsystem:console -opt:ref WindowsPhoneCore.lib RuntimeObject.lib PhoneAppModelHost.lib -NODEFAULTLIB:kernel32.lib -NODEFAULTLIB:ole32.lib" \
-        --prefix=../../../Build/WindowsPhone8.1/x86
+        --prefix=$DIR/Target/$2/Release/ffmpeg-$1
         make install
         popd
 
     fi
 
 elif [ "$1" == "Win7" ]; then
-    echo "Make Win7"
+    echo "Make $1"
 
     if [ "$2" == "x86" ]; then
-        echo "Make Win7 x86"
+        echo "Make $1 $2"
         pushd $DIR/ffmpeg
-        rm -rf Output/Windows7/x86
-        mkdir -p Output/Windows7/x86
-        cd Output/Windows7/x86
-        ../../../configure \
+        rm -rf $DIR/Build/$2/Release/ffmpeg-$1
+        mkdir -p $DIR/Build/$2/Release/ffmpeg-$1
+        cd $DIR/Build/$2/Release/ffmpeg-$1
+        $DIR/ffmpeg/configure \
         --toolchain=msvc \
         --disable-programs \
         --disable-d3d11va \
         --disable-dxva2 \
         --disable-doc \
-        --arch=x86 \
+        --arch="${arch[$2]}" \
         --enable-shared \
         --enable-cross-compile \
         --enable-debug \
         --target-os=win32 \
         --extra-cflags="-MD -D_WINDLL" \
-        --extra-ldflags="-APPCONTAINER:NO -MACHINE:x86" \
-        --prefix=../../../Build/Windows7/x86
+        --extra-ldflags="-APPCONTAINER:NO -MACHINE:$2" \
+        --prefix=$DIR/Target/$2/Release/ffmpeg-$1
         make install
         popd
 
     elif [ "$2" == "x64" ]; then
-        echo "Make Win7 x64"
+        echo "Make $1 $2"
         pushd $DIR/ffmpeg
-        rm -rf Output/Windows7/x64
-        mkdir -p Output/Windows7/x64
-        cd Output/Windows7/x64
-        ../../../configure \
+        rm -rf $DIR/Build/$2/Release/ffmpeg-$1
+        mkdir -p $DIR/Build/$2/Release/ffmpeg-$1
+        cd $DIR/Build/$2/Release/ffmpeg-$1
+        $DIR/ffmpeg/configure \
         --toolchain=msvc \
         --disable-programs \
         --disable-d3d11va \
         --disable-dxva2 \
         --disable-doc \
-        --arch=amd64 \
+        --arch="${arch[$2]}" \
         --enable-shared \
         --enable-cross-compile \
         --enable-debug \
         --target-os=win32 \
         --extra-cflags="-MD -D_WINDLL" \
-        --extra-ldflags="-APPCONTAINER:NO -MACHINE:x64" \
-        --prefix=../../../Build/Windows7/x64
+        --extra-ldflags="-APPCONTAINER:NO -MACHINE:$2" \
+        --prefix=$DIR/Target/$2/Release/ffmpeg-$1
         make install
         popd
 
