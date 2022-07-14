e2359772e2bd1c23d36292d525d0e9205be2b7d9
Lukas Fellechner <lukas.fellechner@gmx.net>
2022-07-14 20:01:58 +0200

    Remove commented code

--- Source/TimeSpanHelpers.h
+++ Source/TimeSpanHelpers.h
@@ -2,53 +2,6 @@
 
 using namespace winrt::Windows::Foundation;
 
-//inline bool operator < (const winrt::Windows::Foundation::TimeSpan& span, const winrt::Windows::Foundation::TimeSpan& other)
-//{
-//	return span.count() < other.count();
-//}
-//inline bool operator <= (const winrt::Windows::Foundation::TimeSpan& span, const winrt::Windows::Foundation::TimeSpan& other)
-//{
-//	return span.count() <= other.count();
-//}
-//inline bool operator>(const winrt::Windows::Foundation::TimeSpan& span, const winrt::Windows::Foundation::TimeSpan& other)
-//{
-//	return span.count() > other.count();
-//}
-//inline bool operator>=(const winrt::Windows::Foundation::TimeSpan& span, const winrt::Windows::Foundation::TimeSpan& other)
-//{
-//	return span.count() >= other.count();
-//}
-//inline bool operator==(const winrt::Windows::Foundation::TimeSpan& span, const winrt::Windows::Foundation::TimeSpan& other)
-//{
-//	return span.count() == other.count();
-//}
-//inline bool operator!=(const winrt::Windows::Foundation::TimeSpan& span, const winrt::Windows::Foundation::TimeSpan& other)
-//{
-//	return span.count() != other.count();
-//}
-//
-//inline winrt::Windows::Foundation::TimeSpan operator+(const winrt::Windows::Foundation::TimeSpan& span, const winrt::Windows::Foundation::TimeSpan& other)
-//{
-//	winrt::Windows::Foundation::TimeSpan result(span.count() + other.count());
-//	return result;
-//}
-//
-//inline winrt::Windows::Foundation::TimeSpan operator-(const winrt::Windows::Foundation::TimeSpan& span, const winrt::Windows::Foundation::TimeSpan& other)
-//{
-//	winrt::Windows::Foundation::TimeSpan result(span.count() - other.count());
-//	return result;
-//}
-//
-//inline winrt::Windows::Foundation::TimeSpan operator += (const winrt::Windows::Foundation::TimeSpan& span, const winrt::Windows::Foundation::TimeSpan& other)
-//{
-//	return winrt::Windows::Foundation::TimeSpan(span.count() + other.count());
-//}
-//
-//inline winrt::Windows::Foundation::TimeSpan operator-=(const winrt::Windows::Foundation::TimeSpan& span, const winrt::Windows::Foundation::TimeSpan& other)
-//{
-//	return winrt::Windows::Foundation::TimeSpan(span.count() - other.count());
-//}
-
 inline winrt::Windows::Foundation::TimeSpan operator*(const winrt::Windows::Foundation::TimeSpan& span, const double factor)
 {
     auto mult = (long long)(span.count() * factor);
