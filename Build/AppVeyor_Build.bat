16a849ba60cb5730fe7160c3020c5aebbd5f147b
Lukas Fellechner <lukas.fellechner@gmx.net>
2022-09-18 17:39:21 +0200

    Fix AppVeyor build

--- Build/AppVeyor_Build.bat
+++ Build/AppVeyor_Build.bat
@@ -4,7 +4,7 @@ nuget restore FFmpegInteropX.sln
 
 msbuild FFmpegInteropX.sln /m /t:build /p:Configuration=Debug;Platform=x86;AppxBundle=Never /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll" || exit
 
-msbuild Source\FFmpegInteropX.DotNet.csproj /t:build /p:Configuration=Release;Platform=AnyCPU;AppxBundle=Never;WINUI=1 /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll" || exit
+msbuild Source\FFmpegInteropX.DotNet.csproj /restore /t:build /p:Configuration=Release;Platform=AnyCPU;AppxBundle=Never;WINUI=1 /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll" || exit
 
 msbuild Source\FFmpegInteropX.vcxproj /t:build /p:Configuration=Release;Platform=x86;AppxBundle=Never /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll" || exit
 
