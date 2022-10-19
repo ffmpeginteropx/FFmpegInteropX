d2e2f2255ad19945bbbb8a4db6ae4b2553d51bc1
Lukas Fellechner <lukas.fellechner@gmx.net>
2022-10-19 21:34:17 +0200

    Fix unit test project

--- FFmpegInteropX.UnitTests/CreateDeleteMediaSource.cs
+++ FFmpegInteropX.UnitTests/CreateDeleteMediaSource.cs
@@ -8,10 +8,25 @@ namespace FFmpegInteropX.UnitTests
     [TestClass]
     public class CreateDeleteMediaSource
     {
+        [STAThread]
+        public static async Task Main(string[] args)
+        {
+            await new CreateDeleteMediaSource().CreateDeleteFromUri();
+        }
+
         [TestMethod]
         public async Task CreateDeleteFromUri()
         {
             var source = await FFmpegMediaSource.CreateFromUriAsync("https://samples.mplayerhq.hu/Matroska/subtitles/multiple_sub_sample.mkv");
+            Assert.IsNotNull(source);
+            source.Dispose();
+        }
+
+
+        [TestMethod]
+        public async Task Grabber()
+        {
+            var source = await FrameGrabber.CreateFromUriAsync("https://samples.mplayerhq.hu/Matroska/subtitles/multiple_sub_sample.mkv");
             Assert.IsNotNull(source);
             source.Dispose();
         }
