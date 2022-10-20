03ea3553ec6bccca0ac1ee709f65793c97d8593d
Lukas Fellechner <lukas.fellechner@gmx.net>
2022-10-20 21:45:16 +0200

    Disable building of NuGet package in AppVeyor to speedup CI

--- AppVeyor_Build.bat
+++ AppVeyor_Build.bat
@@ -6,14 +6,14 @@ msbuild FFmpegInteropX.sln /m /t:build /p:Configuration=Debug;Platform=x64;AppxB
 
 dotnet test .\FFmpegInteropX.UnitTests\bin\x64\Debug\net6.0-windows10.0.22000.0\FFmpegInteropX.UnitTests.dll || exit
 
-msbuild Source\FFmpegInteropX.DotNet.csproj /restore /t:build /p:Configuration=Release;Platform=AnyCPU;AppxBundle=Never;WINUI=1 /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll" || exit
+REM msbuild Source\FFmpegInteropX.DotNet.csproj /restore /t:build /p:Configuration=Release;Platform=AnyCPU;AppxBundle=Never;WINUI=1 /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll" || exit
 
-msbuild Source\FFmpegInteropX.vcxproj /t:build /p:Configuration=Release;Platform=x86;AppxBundle=Never /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll" || exit
+REM msbuild Source\FFmpegInteropX.vcxproj /t:build /p:Configuration=Release;Platform=x86;AppxBundle=Never /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll" || exit
 
-msbuild Source\FFmpegInteropX.vcxproj /t:build /p:Configuration=Release;Platform=x64;AppxBundle=Never /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll" || exit
+REM msbuild Source\FFmpegInteropX.vcxproj /t:build /p:Configuration=Release;Platform=x64;AppxBundle=Never /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll" || exit
 
-msbuild Source\FFmpegInteropX.vcxproj /t:build /p:Configuration=Release;Platform=ARM;AppxBundle=Never /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll" || exit
+REM msbuild Source\FFmpegInteropX.vcxproj /t:build /p:Configuration=Release;Platform=ARM;AppxBundle=Never /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll" || exit
 
-msbuild Source\FFmpegInteropX.vcxproj /t:build /p:Configuration=Release;Platform=ARM64;AppxBundle=Never /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll" || exit
+REM msbuild Source\FFmpegInteropX.vcxproj /t:build /p:Configuration=Release;Platform=ARM64;AppxBundle=Never /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll" || exit
 
-nuget pack Build\FFmpegInteropX.nuspec -Properties id=FFmpegInteropX -Version %1 -Symbols -SymbolPackageFormat snupkg
+REM nuget pack Build\FFmpegInteropX.nuspec -Properties id=FFmpegInteropX -Version %1 -Symbols -SymbolPackageFormat snupkg
