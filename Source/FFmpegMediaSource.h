f77d1627f084e09d924a2f5e6ecebbcaf657ec0c
Lukas Fellechner <lukas.fellechner@gmx.net>
2022-10-25 20:47:00 +0200

    Auto extend media duration if stream contains additional data

--- Source/FFmpegMediaSource.h
+++ Source/FFmpegMediaSource.h
@@ -171,7 +171,7 @@ namespace winrt::FFmpegInteropX::implementation
         void OnPresentationModeChanged(MediaPlaybackTimedMetadataTrackList  const& sender, TimedMetadataPresentationModeChangedEventArgs  const& args);
         void InitializePlaybackItem(MediaPlaybackItem  const& playbackitem);
         bool CheckUseHardwareAcceleration(AVCodecContext* avCodecCtx, HardwareAccelerationStatus const& status, HardwareDecoderStatus& hardwareDecoderStatus, int maxProfile, int maxLevel);
-
+        void CheckExtendDuration(MediaStreamSample sample);
 
     public://internal:
         static winrt::com_ptr<FFmpegMediaSource> CreateFromStream(IRandomAccessStream const& stream, winrt::com_ptr<MediaSourceConfig> const& config, DispatcherQueue  const& dispatcher);
@@ -245,6 +245,8 @@ namespace winrt::FFmpegInteropX::implementation
 
         bool isFirstSeek;
         bool isFirstSeekAfterStreamSwitch = false;
+
+        int lastDurationExtension = 0;
 
         TimeSpan currentPosition{ 0 };
         TimeSpan lastPosition{ 0 };
