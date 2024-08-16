51ab95a890fa1ee677e0e028b527773e0f464cc2
Lukas Fellechner <lukas.fellechner@gmx.net>
2024-08-16 22:13:22 +0200

    Fix wrong build config in appveyor script

--- AppVeyor_Build.bat
+++ AppVeyor_Build.bat
@@ -4,9 +4,9 @@ nuget restore FFmpegInteropX.sln
 
 REM msbuild FFmpegInteropX.sln /m /t:build /p:Configuration=Debug;Platform=x64;AppxBundle=Never /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll" || exit
 
-msbuild FFmpegInteropX.sln /m /restore /t:build /p:Configuration=Debug_WinUI /p:Platform=x64 /p:AppxBundle=Never /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll" || exit
+msbuild FFmpegInteropX.sln /m /restore /t:build /p:Configuration=Debug_Desktop /p:Platform=x64 /p:AppxBundle=Never /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll" || exit
 
-vstest.console /logger:Appveyor Output\FFmpegInteropX.UnitTests\x64\Debug_WinUI\FFmpegInteropX.UnitTests.dll || exit
+vstest.console /logger:Appveyor Output\FFmpegInteropX.UnitTests\x64\Desktop\FFmpegInteropX.UnitTests.dll || exit
 
 REM msbuild Source\FFmpegInteropX.DotNet.csproj /restore /t:build /p:Configuration=Release;Platform=AnyCPU;AppxBundle=Never;WINUI=1 /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll" || exit
 
