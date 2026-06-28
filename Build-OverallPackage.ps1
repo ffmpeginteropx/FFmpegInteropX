f79ae0776472f560edb0e4d533a3cbb95d42e79b
Lukas Fellechner <lukas.fellechner@gmx.net>
2026-06-28 22:50:44 +0200

    Add uwp overall package stuff

--- Build-OverallPackage.ps1
+++ Build-OverallPackage.ps1
@@ -9,6 +9,9 @@ param(
     # If a version string is specified, a NuGet package will be created.
     [string] $LibPackageVersion,
 
+    [ValidateSet('UWP', 'Desktop')]
+    [string[]] $WindowsTargets = ('Desktop', 'UWP'),
+
     [version] $WindowsTargetPlatformVersion = '10.0.22000.0',
 
     # FFmpegInteropX NuGet settings
@@ -19,9 +22,16 @@ param(
     [string] $FFmpegInteropXCommit = $(git --git-dir Libs/ffmpeg/.git rev-parse HEAD)
 )
 
+if ($WindowsTargets -contains 'Desktop') {
+    nuget pack .\Build\FFmpegInteropX.nuspec `
+        -Properties "id=FFmpegInteropX;repositoryUrl=$FFmpegInteropXUrl;repositoryCommit=$FFmpegInteropXCommit;winsdk=$WindowsTargetPlatformVersion;libversion=$LibPackageVersion;ffmpegversion=$FFmpegPackageVersion;NoWarn=NU5128" `
+        -Version $NugetPackageVersion `
+        -OutputDirectory "${PSScriptRoot}\Output\NuGet" `
+}
 
-nuget pack .\Build\FFmpegInteropX.nuspec `
-    -Properties "id=FFmpegInteropX;repositoryUrl=$FFmpegInteropXUrl;repositoryCommit=$FFmpegInteropXCommit;winsdk=$WindowsTargetPlatformVersion;libversion=$LibPackageVersion;ffmpegversion=$FFmpegPackageVersion;NoWarn=NU5128" `
-    -Version $NugetPackageVersion `
-    -OutputDirectory "${PSScriptRoot}\Output\NuGet" `
-	
+if ($WindowsTargets -contains 'UWP') {
+    nuget pack .\Build\FFmpegInteropX.UWP.nuspec `
+        -Properties "id=FFmpegInteropX.UWP;repositoryUrl=$FFmpegInteropXUrl;repositoryCommit=$FFmpegInteropXCommit;winsdk=$WindowsTargetPlatformVersion;libversion=$LibPackageVersion;ffmpegversion=$FFmpegPackageVersion;NoWarn=NU5128" `
+        -Version $NugetPackageVersion `
+        -OutputDirectory "${PSScriptRoot}\Output\NuGet" `
+}
